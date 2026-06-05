9918038eb562f6b017b54cae3484255df410de05
Patrick Ryan <pjryan126@gmail.com>
2026-06-05 13:05:14 -0400

    fix: resolve ADC auth issues for Terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "google_secret_manager_secret" "linkup_api_key" {
     secret_id = "linkup-api-key"
     
     replication {
-        automatic = true
+        auto {}
     }
 }
 
@@ -24,23 +24,24 @@ resource "google_artifact_registry_repository" "mcp_linkedin_bridge_repo" {
 resource "google_cloud_run_service" "mcp_bridge" {
     name = var.service_name
     location = var.region
-    description = "LinkedIn Bridge MCP Server"
     
     template {
-        containers {
-            # This URL follows: {REGION}-docker.pkg.dev/{PROJECT}/{REPO}/{IMAGE}:{TAG}
-            image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.mcp_linkedin_bridge_repo.repository_id}/mcp-linkedin-bridge:latest"
-            
-            env {
-                name = "LINKUP_API_KEY"
-                value_from {
-                    secret_key_ref {
-                        name = google_secret_manager_secret.linkup_api_key.secret_id
-                        key = "latest"
+        spec {
+            containers {
+                # This URL follows: {REGION}-docker.pkg.dev/{PROJECT}/{REPO}/{IMAGE}:{TAG}
+                image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.mcp_linkedin_bridge_repo.repository_id}/mcp-linkedin-bridge:latest"
+                
+                env {
+                    name = "LINKUP_API_KEY"
+                    value_from {
+                        secret_key_ref {
+                            name = google_secret_manager_secret.linkup_api_key.secret_id
+                            key = "latest"
+                        }
                     }
                 }
+                
             }
-            
         }
     }
 
@@ -51,7 +52,7 @@ resource "google_cloud_run_service" "mcp_bridge" {
 }
 
 # 4. Allow public access to the Cloud Run service
-resource "google_cloud_run_iam_member" "public_access" {
+resource "google_cloud_run_service_iam_member" "public" {
     location = google_cloud_run_service.mcp_bridge.location
     service = google_cloud_run_service.mcp_bridge.name
     role = "roles/run.invoker"
